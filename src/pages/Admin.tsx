38ee8cd490499b50a6abd72ee3d1d0efa357b884
Pavel <pal5555@mail.ru>
2024-03-27 06:45:01 +0300

    admin menu

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,8 +1,29 @@
+import { Button, Container } from "react-bootstrap"
+
  const Admin =  () =>{
     return(
-        <>
-        Admin
-        </>
+        <Container className="d-flex  flex-column">
+            <Button 
+            className="mt-3"
+            variant={"outline-primary"}
+            >
+                Добавить тип
+            </Button>
+            <Button 
+            className="mt-3"
+            variant={"outline-primary"}
+            >
+                Добавить бренд
+            </Button>
+            <Button 
+            className="mt-3"
+            variant={"outline-primary"}
+            >
+                Добавить устройство
+            </Button>
+
+
+        </Container>
     )
 }
 export default Admin
