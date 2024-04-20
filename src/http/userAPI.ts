0833781922f09efd9d8baada878b3f41e5ce9cd9
pavel <pal5555@mail.ru>
2024-04-20 22:19:21 +0300

    logic autorization

---
+++ src/http/userAPI.ts
@@ -0,0 +1,17 @@
+import { $authHost, $host } from "./index";
+
+export const  registarion = async (email:string, password:string)=>{
+    const response = await $host.post('api/auth/registration', {email, password, role:'ADMIN'})
+    return response
+}
+
+export const  login = async (email:string, password:string)=>{
+    const response = await $host.post('api/auth/login', {email, password, })
+    return response
+}
+/*
+export const  check = async ()=>{
+    const response = await $host.post('api/auth/registration', {email, password, role:'ADMIN'})
+    return response
+}
+*/
