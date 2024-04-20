1533d9cc60ad0901a07a4db3660f23e2cf9500f4
pavel <pal5555@mail.ru>
2024-04-20 21:54:26 +0300

    base sattings

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
-
+import dotenv from  'dotenv'
 import ReactDOM from 'react-dom/client'
 
 import App from './App.tsx'
 import './index.css'
-
-
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <App /> 
 )
