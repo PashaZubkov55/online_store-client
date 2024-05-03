ad66feccad20292ecae44679e4e5ccb37f6a4b9e
Pavel <pal5555@mail.ru>
2024-05-03 15:52:47 +0300

    check login

--- src/http/userAPI.ts
+++ src/http/userAPI.ts
@@ -4,16 +4,19 @@ import { $authHost, $host } from "./index";
 
 export const  registration = async (email:string, password:string)=>{
     const {data}= await $host.post('api/user/registration', {email, password, role:'ADMIN'})
+    localStorage.setItem('token', data.token)
     return jwtDecode(data.token)
 }
 
 
 export const  login = async (email:string, password:string)=>{
     const {data} = await $host.post('api/user/login', {email, password, })
+    localStorage.setItem('token', data.token)
     return jwtDecode(data.token)
 }
 
 export const  check = async ()=>{
-    const response = await $host.post('api/auth/registration')
-    return response
+    const {data} = await $authHost.post('api/auth/auth')
+    localStorage.setItem('token', data.token)
+    return jwtDecode(data.token)
 }
