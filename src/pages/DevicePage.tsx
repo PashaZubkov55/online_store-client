a3b0040d4638521ec1f025126ba46526e844539a
Pavel <pal5555@mail.ru>
2024-10-06 08:47:32 +0300

    'write_data_device_detail_page'

--- src/pages/DevicePage.tsx
+++ src/pages/DevicePage.tsx
@@ -1,27 +1,28 @@
-import { Col, Card, Container, Image, Row, Button } from "react-bootstrap"
-import { Idevices } from "../utils/interface"
-import Star1 from '../assets/img/Star1.png'
+import {useEffect, useState} from 'react';
+import {Button, Card, Col, Container, Image, Row} from "react-bootstrap";
+import bigStar from '../assets/img/Star1.png'
+import {useParams} from 'react-router-dom'
+import {fetchOneDevice} from "../http/deviceAPI";
 
- const DevicePage =  () =>{
-    const device:Idevices =  {
-        id:1,
-        name:'9c',
-        price:10000,
-        rating:5,
-        img:'https://cdn1.ozone.ru/s3/multimedia-u/6379580718.jpg'
-    }
-    return(
+const DevicePage = () => {
+    const [device, setDevice] = useState({info: []})
+    const {id} = useParams()
+    useEffect(() => {
+        fetchOneDevice(id).then(data => setDevice(data))
+    }, [])
+
+    return (
         <Container className="mt-3">
-           <Row>
+            <Row>
                 <Col md={4}>
-                    <Image width={300} height={300} src={device.img}/>
+                    <Image width={300} height={300} src={'http://localhost:4000/'+ device.img}/>
                 </Col>
                 <Col md={4}>
-                    <Row className="d-flex flex-column align-items-center text-center">
+                    <Row className="d-flex flex-column align-items-center">
                         <h2>{device.name}</h2>
                         <div
-                            className="rating__icon d-flex align-items-center justify-content-center "
-                            style={{background: `url(${Star1}) no-repeat center center`, width:240, height: 240, backgroundSize: 'cover', fontSize:64}}
+                            className="d-flex align-items-center justify-content-center"
+                            style={{background: `url(${bigStar}) no-repeat center center`, width:240, height: 240, backgroundSize: 'cover', fontSize:64}}
                         >
                             {device.rating}
                         </div>
@@ -33,16 +34,20 @@ import Star1 from '../assets/img/Star1.png'
                         style={{width: 300, height: 300, fontSize: 32, border: '5px solid lightgray'}}
                     >
                         <h3>От: {device.price} руб.</h3>
-                        <Button variant={"outline-primary"}>Добавить в корзину</Button>
+                        <Button variant={"outline-dark"}>Добавить в корзину</Button>
                     </Card>
                 </Col>
             </Row>
             <Row className="d-flex flex-column m-3">
                 <h1>Характеристики</h1>
-                
+                {device.info.map((info, index) =>
+                    <Row key={info.id} style={{background: index % 2 === 0 ? 'lightgray' : 'transparent', padding: 10}}>
+                        {info.title}: {info.description}
+                    </Row>
+                )}
             </Row>
         </Container>
-    )
-}
+    );
+};
 
-export default DevicePage
+export default DevicePage;
